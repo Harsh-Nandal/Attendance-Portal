c7a8e9debb2110cb067d6ccad076fc5fc54e4733
Harsh-Nandal <harshnandal862005@gmail.com>
2025-08-13 22:38:04 +0530

    solving npm run build error

--- pages/api/admin/student.js
+++ pages/api/admin/student.js
@@ -1,5 +1,6 @@
-import connectDB from "../../../../lib/mongodb";
-import Attendance from "../../../../models/Attendance";
+import connectDB from "../../../lib/mongodb";
+import Attendance from "../../../models/Attendance";
+
 
 export default async function handler(req, res) {
   if (req.method !== "GET") {
--- pages/api/admin/student/[id].js
+++ pages/api/admin/student/[id].js
@@ -1,6 +1,7 @@
 import connectDB from "../../../../lib/mongodb";
 import Attendance from "../../../../models/Attendance";
 
+
 export default async function handler(req, res) {
   if (req.method !== "GET") {
     return res.status(405).json({ message: "Method Not Allowed" });
